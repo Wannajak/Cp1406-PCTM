b157423a7de5a44b2d09123aec4a5f7b883876ce
Wannajak <Fu@Dariuss-MacBook-Pro.local>
2016-04-26 15:11:37 +1000

    m1
    summit for first milestone

---
+++ Web/web/manu.js
@@ -0,0 +1,25 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+document.write(' <div id="logo"><img src="images/Logo.jpg" width="900" height="142" alt=""/></div>');
+document.write(' <link rel="stylesheet" type="text/css" href="style.css"/>');
+document.write(' <div id="container">');
+document.write(' <nav> <ul>');
+document.write(' <li><a href="index.html">Home</a>\n\</li>');
+
+document.write(' <li><a>Gallery</a><ul>');
+document.write(' </ul></li>');
+
+document.write(' <li><a href="supporters.html">Supporters</a><ul>');
+document.write(' </ul></li>');
+
+document.write(' <li><a>People</a></li>');
+
+
+document.write(' <li><a>Abount Us</a><ul>');
+document.write(' </ul></li>');
+
+document.write(' </ul></nav><br/></div>');
+
