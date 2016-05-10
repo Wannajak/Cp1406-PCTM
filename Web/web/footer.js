0f91e0f5d52e181af74b9a5472d98b3434e9ed14
Wannajak <Fu@Dariuss-MacBook-Pro.local>
2016-05-10 13:08:01 +1000

    footer,link,tab
    move footer to js
    add all manu link
    add tab in gallery

---
+++ Web/web/footer.js
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+document.write(' <div id="footer"> ');
+document.write(' <div class="footer"> ');
+document.write(' <fieldset> ');
+document.write(' <legend><h2>Contact us</h2> </legend> ');
+document.write(' <p>North Queensland Ensembles Inc. is an Incorporated Association registered in the State of Queensland. It is also a tax deductible fund listed on the Register of Cultural Organisations under Subdivision 30-B of the Income tax Assessment Act 1997. It is based in Townsville, North Queensland, Australia.</p> ');
+document.write(' <h2>Or send us a message <a><strong><u>Here</u></storng</a></h2> ');
+document.write(' </fieldset> ');
+document.write(' </div></div> ');
+
+
+
+
+
--- Web/web/manu.js
+++ Web/web/manu.js
@@ -5,21 +5,20 @@
  */
 document.write(' <div id="logo"><img src="images/Logo.jpg" width="900" height="142" alt=""/></div>');
 document.write(' <link rel="stylesheet" type="text/css" href="style.css"/>');
-document.write(' <div id="container">');
+document.write(' <div id="header">');
 document.write(' <nav> <ul>');
 document.write(' <li><a href="index.html">Home</a>\n\</li>');
 
-document.write(' <li><a>Gallery</a><ul>');
+document.write(' <li><a href="gallery.html">Gallery</a><ul>');
 document.write(' </ul></li>');
 
 document.write(' <li><a href="supporters.html">Supporters</a><ul>');
 document.write(' </ul></li>');
 
-document.write(' <li><a>People</a></li>');
+document.write(' <li><a href="people.html">People</a></li>');
 
 
-document.write(' <li><a>Abount Us</a><ul>');
+document.write(' <li><a href="aboutus.html">Abount Us</a><ul>');
 document.write(' </ul></li>');
 
 document.write(' </ul></nav><br/></div>');
-
