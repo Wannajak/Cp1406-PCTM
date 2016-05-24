418334ba526f28e61e005fdca97223e8e8a15246
Wannajak <fou1234@hotmail.com>
2016-05-25 02:27:32 +1000

    gallery,Facebook,google form

--- Web/web/js/footer.js
+++ Web/web/js/footer.js
@@ -9,7 +9,7 @@ document.write(' <div class="footer"> ');
 document.write(' <fieldset> ');
 document.write(' <legend><h2>Contact us</h2> </legend> ');
 document.write(' <p>North Queensland Ensembles Inc. is an Incorporated Association registered in the State of Queensland. It is also a tax deductible fund listed on the Register of Cultural Organisations under Subdivision 30-B of the Income tax Assessment Act 1997. It is based in Townsville, North Queensland, Australia.</p> ');
-document.write(' <h2>Or send us a message <a><strong><u>Here</u></storng</a></h2> ');
+document.write(' <h2>Or send us a message <a  href="https://docs.google.com/forms/d/154Z3al8bgaXl0KviXW3O2l8OzJ-iqP9LIvZ1-ICI-Jo/prefill"><strong><u>Here</strong></u></a></h2> ');
 document.write(' </fieldset> ');
 document.write(' </div></div> ');
 
---
+++ Web/web/js/manu.js
@@ -0,0 +1,22 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+document.write(' <div class="logo"><img src="images/Logo.jpg" id="logo"></div>');
+document.write(' <div class="header">');
+document.write(' <label for="show-menu" class="show-menu">Show Menu</label>');
+document.write(' <input type="checkbox" id="show-menu" role="button">');
+document.write(' <ul id = "menu">');
+document.write(' <li><a href="index.html">Home</a></li>');
+
+document.write(' <li><a href="gallery.html">Gallery</a></li>');
+
+document.write(' <li><a href="supporters.html">Supporters</a></li>');
+
+document.write(' <li><a href="people.html">People</a></li>');
+
+
+document.write(' <li><a href="aboutus.html">Abount Us</a></li>');
+
+document.write(' </ul><br/></div>');
