2be3afb12e03268f831316ad490c73dffdc3f797
Wannajak <fou1234@hotmail.com>
2016-05-27 03:52:12 +1000

    final

--- Web/web/js/manu.js
+++ Web/web/js/manu.js
@@ -8,20 +8,20 @@ document.write(' <div class="header">');
 document.write(' <label for="show-menu" class="show-menu">Show Menu</label>');
 document.write(' <input type="checkbox" id="show-menu" role="button">');
 document.write(' <ul id = "menu">');
-document.write(' <li id = "subbar"><a href="index.html">Home</a></li>');
+document.write(' <li class = "subbar"><a href="index.html">Home</a></li>');
 
-document.write(' <li id = "subbar"><a href="#">People</a>');
+document.write(' <li class = "subbar"><a href="#">People</a>');
 document.write(' <ul class="hidden">');
 document.write(' <li><a href="member.html">Member</a></li>');
 document.write(' <li><a href="specialguest.html">Specical Guest</a></li>');
 document.write(' <li><a href="supporters.html">Supporters</a></li>');
 document.write(' </ul></li>');
 
-document.write(' <li id = "subbar"><a href="news.html">News</a></li>');
+document.write(' <li class = "subbar"><a href="news.html">News</a></li>');
 
-document.write(' <li id = "subbar"><a href="gallery.html">Gallery</a></li>');
+document.write(' <li class = "subbar"><a href="gallery.html">Gallery</a></li>');
 
-document.write(' <li id = "subbar"><a href="#">About Us</a>');
+document.write(' <li class = "subbar"><a href="#">About Us</a>');
 document.write(' <ul class="hidden">');
 document.write(' <li><a href="history.html">History</a></li>');
 document.write(' <li><a href="about.html">About</a></li>');
